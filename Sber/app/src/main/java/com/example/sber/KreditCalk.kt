ecf8e50e2a819ed1824c05d172d8713b2384f094
katya1331 <152582001+katya1331@users.noreply.github.com>
2024-09-03 15:30:43 +0500

    Add files via upload

---
+++ Sber/app/src/main/java/com/example/sber/KreditCalk.kt
@@ -0,0 +1,11 @@
+package com.example.sber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KreditCalk : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kredit_calk)
+    }
+}
---
+++ Sber/app/src/main/java/com/example/sber/Raschet.kt
@@ -0,0 +1,11 @@
+package com.example.sber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Raschet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_raschet)
+    }
+}
